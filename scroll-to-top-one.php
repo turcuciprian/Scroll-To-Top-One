389b97bf2dcb25bbf7255e5f771fdbbaca0546a9
Ciprian Turcu <turcuciprian1@gmail.com>
2016-06-07 00:07:04 +0300

    updated readme and main plugin information

--- scroll-to-top-one.php
+++ scroll-to-top-one.php
@@ -2,7 +2,7 @@
 /*
   Plugin Name: Scroll To Top One
   Plugin URI: http://admin-builder.com
-  Description: Add a custom/dinamic scroll to top button to your site
+  Description: Add a custom/dinamic scroll to top button to your site, configure background style (round or rounded, configure color and upload your own icon for the up graphic)
   Version: 1.0
   Author: rootabout
   Author URI: http://admin-builder.com
